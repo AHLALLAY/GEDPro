bae82eaa70045d0d737c7e162ceee906f34bfbdf
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-25 00:27:56 +0100

    feat: Configuration base de données MongoDB et PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { mongodbConfig, postgresConfig } from './database/config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(mongodbConfig.uri || 'mongodb://localhost:27017/gedpro', {}),
+    TypeOrmModule.forRoot({
+      type: 'postgres' as const,
+      host: postgresConfig.host,
+      port: postgresConfig.port ? parseInt(postgresConfig.port, 10) : 5432,
+      username: postgresConfig.username,
+      password: postgresConfig.password,
+      database: postgresConfig.database,
+      entities: ['/**/*.entity{.ts,.js}'],
+      migrations: ['../database/migrations/*']
+    }),
+    DatabaseModule,
+    UserModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/database/config/database.config.ts
@@ -0,0 +1,12 @@
+export const mongodbConfig = {
+    uri: process.env.MONGODB_URI
+}
+
+export const postgresConfig = {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+}
