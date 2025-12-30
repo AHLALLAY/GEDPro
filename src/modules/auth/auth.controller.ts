bc848ff7adccc77af82b92120dbb882ef1065f87
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2025-12-31 00:35:18 +0100

    chore: Configuration du préfixe global '/api' et simplification des routes auth

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
 
-@Controller('api/auth')
+@Controller('/auth')
 export class AuthController {
     constructor (private readonly authService: AuthService) {}
 
